0be0f9ef2430138b7b90817c3c7d856fff26e692
chamindujs <chamindujs@gmail.com>
2021-06-04 18:51:33 +0530

    Added 404 page

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -7,7 +7,7 @@ function NotFound() {
         <div className="err">4</div>
         <i className="far fa-question-circle fa-spin" />
         <div className="err2">4</div>
-        <div className="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?<p>Let's go <a href="#">home</a> and try from there.</p></div>
+        <div className="msg">Looks like you entered a wrong URL buddy, Please check it again<p>Let's go to the <a href="/">home page</a>  and try from there </p></div>
       </div>
         </div>
     )
