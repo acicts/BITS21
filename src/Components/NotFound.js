bffa99cf61244275dbd8386da5f62a59069f20b2
chamindujs <chamindujs@gmail.com>
2021-06-07 19:45:02 +0530

    Responsive 404 page

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -2,14 +2,17 @@ import React from 'react'
 
 function NotFound() {
     return (
-        <div>
+       
             <div className="mainbox">
-        <div className="err">4</div>
-        <i className="far fa-question-circle fa-spin" />
-        <div className="err2">4</div>
-        <div className="msg">Looks like you entered a wrong URL buddy, Please check it again<p>Let's go to the <a href="/">home page</a>  and try from there </p></div>
-      </div>
-        </div>
+                <div className="404">
+                    <div className="err">4</div>
+                        <i className="far fa-question-circle fa-spin" />
+                    <div className="err2">4</div>
+                </div>
+        
+             <div className="msg">Looks like you entered a wrong URL buddy, Please check it again<p>Let's go to the <a href="/">home page</a>  and try from there </p></div>
+            </div>
+    
     )
 }
 
