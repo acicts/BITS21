59c669130114dad7cf8967f561d64e5fd2c9368c
chamindujs <chamindujs@gmail.com>
2021-06-10 19:08:32 +0530

    Add quotes

--- src/Components/Home/Quotes.js
+++ src/Components/Home/Quotes.js
@@ -4,16 +4,27 @@ import Typography from '@material-ui/core/Typography';
 
 function Quotes() {
     const useStyles = makeStyles({
+        quoteContainer: {
+            padding: '80px',
+            
+        }
     });
 
     const classes = useStyles();
     return (
         <div>
             <div className={classes.quoteContainer}>
-                <Typography align='left' variant='h4'>BITS is the best thing that ever happened to me!</Typography>
-                <Typography align='left' variant='h6' >~ Chamindu Jayanath</Typography>
-                <Typography align='left' variant='caption' >BITS 2019 Winner</Typography>
+                <Typography align='left' variant='h4'> <span style={{color: '#3ed1b8', fontSize: '50px'}}>“</span> BITS is the best thing that ever happened to me! <span style={{color: '#3ed1b8', fontSize: '50px'}}>”</span> </Typography>
+                <Typography align='left' variant='h6' style={{marginLeft: '30px'}}>~ Chamindu Jayanath</Typography>
+                <Typography align='left' variant='h6' style={{marginLeft: '45px', color: '#ffffffb3'}}>BITS 2019 Winner</Typography>
             </div>
+
+            <div className={classes.quoteContainer}>
+                <Typography align='right' variant='h4'> <span style={{color: '#3ed1b8', fontSize: '50px'}}>“</span> I think BITS will have a great impact on the students who like computers <span style={{color: '#3ed1b8', fontSize: '50px'}}>”</span> </Typography>
+                <Typography align='right' variant='h6' style={{marginRight: '30px'}}>~ Chamuthu Edirisuriya</Typography>
+                <Typography align='right' variant='h6' style={{marginRight: '45px', color: '#ffffffb3'}}>President of ACICTS</Typography>
+            </div>
+
         </div>
     )
 }
