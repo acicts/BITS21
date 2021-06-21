26da6cef2ca3fabf5f200cdf3b7933c7a32a2d93
chamindujs <chamindujs@gmail.com>
2021-06-21 19:03:36 +0530

    Remove footer from router

--- src/App.js
+++ src/App.js
@@ -42,7 +42,6 @@ export default function App() {
       <Route  path='*'> <NotFound /> </Route>
       <Route  path='*/*'> <NotFound /> </Route>
       </Switch>
-      <Footer />
       </ThemeProvider>
     
     </div>
