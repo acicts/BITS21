29e901f2627ef512d7841d33e4d71a907521252a
Chamindu Amarasinghe <chamindujs@gmail.com>
2021-08-11 14:33:44 +0530

    Update onlineTest.js

--- src/Components/OnlineTest/onlineTest.js
+++ src/Components/OnlineTest/onlineTest.js
@@ -165,7 +165,7 @@ export default function TaskCard() {
         
         <div className={classes.controls}>
         <Button target="blank" variant="contained" className={classes.button}>
-          The test has been ended
+          The test has ended
         </Button>
         
         </div>
