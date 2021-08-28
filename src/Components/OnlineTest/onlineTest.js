292b7d52c6b9e788ebc1bf3c538c50c71299ca2f
Chamindu Amarasinghe <chamindujs@gmail.com>
2021-08-28 23:58:02 +0530

    Create onlineTest.js

--- src/Components/OnlineTest/onlineTest.js
+++ src/Components/OnlineTest/onlineTest.js
@@ -232,8 +232,12 @@ export default function TaskCard() {
         </CardContent>
         
         <div className={classes.controls}>
-        <Button target="blank" variant="contained" href="#" className={classes.button}>
-        Test is not available yet
+         <Button target="blank" variant="contained" href="https://quilgo.com/form/HNOrjomFlFe43yxu" className={classes.button}>
+        Grade 06 - 09
+        </Button>
+
+        <Button target="blank" variant="contained" href="https://quilgo.com/form/TxyLg3FyVfb25uFl" className={classes.button}>
+        Grade 10 - Post O/L
         </Button>
         
         </div>
