932cf25335ca0dcf71603318511e6f38431fac25
chamindujs <chamindujs@gmail.com>
2021-09-02 20:53:23 +0530

    Update header

--- src/Components/Home/Header.js
+++ src/Components/Home/Header.js
@@ -55,9 +55,9 @@ export default function Header() {
                 <Grid item  xl={6} lg={6} md={12} sm={12} xs={12} className={classes.hero}>
                     <Typography variant='h2' gutterBottom="true" align='left' style={{marginBottom: '50px', fontWeight: 'bold'}}>BITS 2021</Typography>
 
-                    <div className="countdown">The Glory Awaits You At The Finish Line
+                    <div className="countdown">Winners have been announced!
                     <br></br>
-                    Winners announcing soon
+
                     </div>
 
                     <Typography variant='h5'  align='left' style={{marginBottom: '50px'}}><span style={{color: '#3ed1b8'}}>The First-Ever Task-Based Intra-School ICT competition,</span>  organized by the Ananda College ICT Society to provide a platform for our school's students to polish and improve their ICT knowledge in many fields of ICT</Typography>
